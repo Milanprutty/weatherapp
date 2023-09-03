91a92baf89b42818ec7db06df7447564eee67e93
Milanprutty <milan111x@gmail.com>
2023-09-03 19:12:08 +0200

    -

--- src/app/components/api/WeatherApi.jsx
+++ src/app/components/api/WeatherApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const WeatherApi = async (currentText) => {
   const data = await axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${currentText}&appid=66d6009336828b53d029b4f237899806`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${currentText}&appid=66d6009336828b53d029b4f237899806`
   );
 
   const weatherData = await axios.get(
